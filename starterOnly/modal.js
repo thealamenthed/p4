e5fa30983ec4ba16d3ffc915a9700522348da373
thealamenthed <dalila.boualemfr@gmail.com>
2024-11-26 10:22:50 +0100

    feat(form): implement errorMessages under inputs with Element.closest()

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -34,16 +34,24 @@ function closeModal() {
 2.Empêcher l'envoi du formulaire si les conditions ne sont pas respectées
 3.Vérifier chaque condition de validation et afficher un message d'erreur si nécessaire
 4.Conserver les données saisies même si la validation échoue
+
+Ajouter le message d'erreur sous l'input :
+1.Cibler la zone ou le message doit etre afficher
+2.Vider tous les span errorMessage
+3.Afficher un à un
 */
 
-// Gestion de l'événement submit sur le formulaire.
+// Gestion de l'événement submit sur le formulaire
 let form = document.querySelector("form");
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  // Réinitialiser les messages d'erreur
-  const errorMessages = document.getElementById("errorMessages");
-  errorMessages.innerHTML = " ";
+  // Réinitialiser les messages d'erreurs
+  const errorMessages = document.querySelectorAll(".errorMessage");
+
+  for (i = 0; i < errorMessages.length; i++) {
+    errorMessages[i].innerHTML = "";
+  }
 
   // Collecter les données du formulaire
   const prenom = document.getElementById("first");
@@ -51,46 +59,56 @@ form.addEventListener("submit", (event) => {
   const email = document.getElementById("email");
   const birthdate = document.getElementById("birthdate");
   const quantity = document.getElementById("quantity");
-  const locationChecked = document.querySelector('input[name="location"]');
+  const locationChecked = document.querySelectorAll('input[name="location"]');
   const checkboxCondition = document.getElementById("checkbox1");
 
   const isValid = true;
 
+  // Vérifier chaque condition de validation et afficher un message d'erreur
   if (prenom.value.trim().length < 2) {
-    errorMessages.innerHTML +=
+    prenom.closest(".formData").querySelector(".errorMessage").innerHTML =
       "Veuillez entrer 2 caractères ou plus pour le champ du prénom";
   }
 
   if (nom.value.trim().length < 2) {
-    errorMessages.innerHTML +=
-      "Veuillez entrer 2 caractères ou plus pour le champ du nom.";
+    nom.closest(".formData").querySelector(".errorMessage").innerHTML =
+      "Veuillez entrer 2 caractères ou plus pour le champ du nom";
   }
 
   const emailRegex = /[a-z0-9.-_]+@[a-z0-9.-_]+\.[a-z0-9.-_]+/;
   if (!emailRegex.test(email.value.trim())) {
-    errorMessages.innerHTML += "L'email n'est pas valide";
+    email.closest(".formData").querySelector(".errorMessage").innerHTML =
+      "L'email n'est pas valide";
   }
 
   if (!birthdate.value) {
-    errorMessages.innerHTML +=
-      "Veuillez entrer une date de naissance valide (mois/jour/année).";
+    birthdate.closest(".formData").querySelector(".errorMessage").innerHTML =
+      "Veuillez entrer une date de naissance valide (mois/jour/année)";
   }
-
-  console.log("--------");
-  console.log(quantity.value.length);
-  console.log(email.value.length);
 
   if (quantity.value.length <= 0) {
-    errorMessages.innerHTML += "Veuillez entrer un nombre en chiffres";
+    quantity.closest(".formData").querySelector(".errorMessage").innerHTML =
+      "Veuillez entrer un nombre en chiffres";
   }
 
-  if (!locationChecked.checked) {
-    errorMessages.innerHTML += "Veuillez choisir un tournoi";
+  let checkedFalse = 0;
+  for (i = 0; i < locationChecked.length; i++) {
+    if (!locationChecked[i].checked) {
+      checkedFalse++;
+    }
+  }
+
+  if (checkedFalse === locationChecked.length) {
+    locationChecked[0]
+      .closest(".formData")
+      .querySelector(".errorMessage").innerHTML = "Veuillez choisir un tournoi";
   }
 
   if (!checkboxCondition.checked) {
-    errorMessages.innerHTML +=
-      "Vous devez vérifier que vous acceptez les termes et conditions.";
+    checkboxCondition
+      .closest(".formData")
+      .querySelector(".errorMessage").innerHTML =
+      "Vous devez vérifier que vous acceptez les termes et conditions";
   }
 
   // if (isValid === true) {
